652d13428e6902cc3c95a63ffa51f92e134082d2
Bublik11 <vanya.bitseps@gmail.com>
2022-02-16 15:32:43 +0300

    lesson 3 done!

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,45 +1,43 @@
-import json
 from django.shortcuts import render
+from .models import Product, ProductCategory
 
-# Create your views here.
 
 MAIN_MENU_LINKS = [
     {'url': 'main', 'name': 'домой'},
-    {'url': 'products', 'name': 'продукты'},
+    {'url': 'products:index', 'name': 'продукты'},
     {'url': 'contact', 'name': 'контакты'},
 ]
 
 
-
 def index(request):
+    title = "Главная"
+    products = Product.objects.all()[:4]
     return render(request, 'mainapp/index.html', context= {
-        'title': "Главная",
+        'title': title,
         'main_menu_links': MAIN_MENU_LINKS,
+        'products': products,
     })
 
 
-def products(request):
-    PRODUCTS_MENU_LINKS = [
-        {'url': 'products_all', 'name': 'все'},
-        {'url': 'products_home', 'name': 'дом'},
-        {'url': 'products_office', 'name': 'офис'},
-        {'url': 'products_modern', 'name': 'модерн'},
-        {'url': 'products_classic', 'name': 'классика'},
-    ]
-
-    with open('./products.json', encoding='utf-8') as file:
-        products = json.load(file)
-
+def products(request, pk=None):
+    title = "Продукты"
+    MENU_CATEGORY = ProductCategory.objects.all()
+    if pk is None:
+        products = Product.objects.all()[:3]
+    else:
+        products = Product.objects.filter(category=pk)[:3]
+    
     return render(request, 'mainapp/products.html', context= {
-        'title': "Продукты",
+        'title': title,
         'main_menu_links': MAIN_MENU_LINKS,
-        "products_menu_links": PRODUCTS_MENU_LINKS,
+        "products_menu_links": MENU_CATEGORY,
         "products": products,
     })
 
 
 def contact(request):
+    title = "Контакты"
     return render(request, 'mainapp/contact.html', context= {
-        'title': "Контакты",
+        'title': title,
         'main_menu_links': MAIN_MENU_LINKS,
     })
