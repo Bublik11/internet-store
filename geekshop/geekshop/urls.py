c84076edec5c48ad92a40894d182d1fe281dda89
Bublik11 <vanya.bitseps@gmail.com>
2022-02-17 17:04:57 +0300

    lesson_4 done!

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -21,10 +21,11 @@ from mainapp import views as mainapp
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', mainapp.index, name="main"),
-    path('contact/', mainapp.contact, name="contact"),
-    path('products/',include('mainapp.urls', namespace="products")),
+    path("admin/", admin.site.urls),
+    path("", mainapp.index, name="main"),
+    path("contact/", mainapp.contact, name="contact"),
+    path("products/", include("mainapp.urls", namespace="products")),
+    path("auth/", include("authapp.urls", namespace="auth")),
 ]
 
 if DEBUG:
